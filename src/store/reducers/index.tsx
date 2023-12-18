83510442f3395e258a207d12424f98bce7953ecf
Jake Servin <100403428+JakeServin@users.noreply.github.com>
2023-12-18 16:24:56 -0700

    removed redux

--- src/store/reducers/index.tsx
+++
@@ -1,9 +0,0 @@
-import usersReducer from '@/store/reducers/user-reducer';
-import { combineReducers } from 'redux';
-
-// Combine Reducers
-var reducers = combineReducers({
-    users: usersReducer,
-});
-
-export default reducers;
